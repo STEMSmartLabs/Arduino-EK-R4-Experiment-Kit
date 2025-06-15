4e6ee1e752f2299cc057adcb0dc7b26848501899
Vishal <230638.EC@rmkec.ac.in>
2025-06-15 23:37:20 +0530

    Updated example files

--- examples/sensors/IR/IR.ino
+++ examples/sensors/IR/IR.ino
@@ -1,4 +1,4 @@
-#include <IRremote.hpp>//Include IRremote library version 4.4.2
+#include <IRremote.hpp>//Include IRremote library version: 4.4.2
 
 int RECV_PIN = 6; // Pin connected to the IR receiver
 

77dea275989c611690a5457d3a6f17bb9d349392
R J Vishal <vishalonline2023@gmail.com>
2025-07-03 10:18:53 +0530

    Update smart_name_badge.ino

--- examples/applications/Assistive Technology/smart_name_badge/smart_name_badge.ino
+++ examples/applications/Assistive Technology/smart_name_badge/smart_name_badge.ino
@@ -7,13 +7,12 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 String messages[] = {"STEM Smart Labs", "Hello! I'm Vishal", "Arduino UNO R4"};
 int index = 0;
 
 void showSMLLogo() {
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
   display.drawBitmap(0, 0, SMLimage, 128, 64, 1);
   display.display();
@@ -22,6 +21,7 @@ void showSMLLogo() {
 }
 
 void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // Moved here
   IrReceiver.begin(IR_PIN);
   showSMLLogo();
 }
@@ -29,6 +29,7 @@ void setup() {
 void loop() {
   display.clearDisplay();
   display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);  // Ensure text is visible
   display.setCursor(0, 20);
   display.println(messages[index]);
   display.display();

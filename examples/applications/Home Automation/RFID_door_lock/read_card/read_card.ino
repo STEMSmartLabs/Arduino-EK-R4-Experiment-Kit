14ab5f3e4d65fa6739442e71cb9e1fc5c7f4a34c
R J Vishal <vishalonline2023@gmail.com>
2025-07-03 10:15:24 +0530

    Update read_card.ino

--- examples/applications/Home Automation/RFID_door_lock/read_card/read_card.ino
+++ examples/applications/Home Automation/RFID_door_lock/read_card/read_card.ino
@@ -1,24 +1,52 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <EEPROM.h>
 
-#define SS_PIN 2
-#define RST_PIN 4
+// New pin connections
+#define SS_PIN A2    // RFID SDA connected to A2
+#define RST_PIN A3   // RFID RST connected to A3
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);
+bool uidSaved = false;
 
 void setup() {
   Serial.begin(9600);
+  while (!Serial);  // Wait for Serial Monitor to connect
   SPI.begin();
+  delay(100);
   mfrc522.PCD_Init();
-  Serial.println("Scan a card...");
+
+  Serial.println("Scan a card to save UID...");
 }
 
 void loop() {
-  if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) return;
+  Serial.println("\nWaiting for card...");
+
+  while (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
+    delay(100);
+  }
 
   Serial.print("UID: ");
   for (byte i = 0; i < mfrc522.uid.size; i++) {
-    Serial.print("0x"); Serial.print(mfrc522.uid.uidByte[i], HEX); Serial.print(" ");
+    byte val = mfrc522.uid.uidByte[i];
+    Serial.print("0x");
+    if (val < 0x10) Serial.print("0");
+    Serial.print(val, HEX);
+    Serial.print(" ");
+
+    if (!uidSaved) EEPROM.write(i, val);  // Save only first UID
   }
-  Serial.println();
-  delay(1000);
+
+  if (!uidSaved) {
+    uidSaved = true;
+    Serial.println("\nUID saved to EEPROM.");
+  } else {
+    Serial.println("\nCard scanned.");
+  }
+
+  // Halt and reset
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
+
+  delay(2000);
 }

4244c0b8a703e59e20b3dfa6d59729f2d198b9ca
R J Vishal <vishalonline2023@gmail.com>
2025-07-03 10:21:01 +0530

    Update Analog_Clock.ino

--- examples/applications/Clocks and Scientific Tools/Analog_Clock/Analog_Clock.ino
+++ examples/applications/Clocks and Scientific Tools/Analog_Clock/Analog_Clock.ino
@@ -1,15 +1,22 @@
-#include <Wire.h>
-#include <RTClib.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include "SMLimage.h"
+#include <Wire.h>
+#include <math.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
 
-RTC_DS3231 rtc;
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+const int centerX = SCREEN_WIDTH / 2;
+const int centerY = SCREEN_HEIGHT / 2;
+
+unsigned long previousMillis = 0;
+int hours = 0;     // Start time manually (e.g., 9:10:00 AM)
+int minutes = 0;
+int seconds = 0;
 
 void showSMLLogo() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
@@ -20,43 +27,51 @@ void showSMLLogo() {
   display.clearDisplay();
 }
 
-// Draws a clock hand from centerX, centerY at a given angle and length
-void drawHand(int centerX, int centerY, float angleRad, int length) {
-  int x = centerX + length * cos(angleRad);
-  int y = centerY + length * sin(angleRad);
-  display.drawLine(centerX, centerY, x, y, WHITE);
+void drawHand(float angle, int length, int color) {
+  int x = centerX + length * cos(angle);
+  int y = centerY + length * sin(angle);
+  display.drawLine(centerX, centerY, x, y, color);
+}
+
+void updateTime() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= 1000) {
+    previousMillis = currentMillis;
+    seconds++;
+    if (seconds >= 60) {
+      seconds = 0;
+      minutes++;
+      if (minutes >= 60) {
+        minutes = 0;
+        hours++;
+        if (hours >= 24) {
+          hours = 0;
+        }
+      }
+    }
+  }
 }
 
 void setup() {
-  Wire.begin();
-  rtc.begin();
   showSMLLogo();
-  display.setTextColor(WHITE);
 }
 
 void loop() {
-  DateTime now = rtc.now();
+  updateTime();
 
-  int centerX = SCREEN_WIDTH / 2;
-  int centerY = SCREEN_HEIGHT / 2;
-
-  float secondAngle = now.second() * 6 * PI / 180 - PI / 2;
-  float minuteAngle = now.minute() * 6 * PI / 180 - PI / 2;
-  float hourAngle = ((now.hour() % 12) + now.minute() / 60.0) * 30 * PI / 180 - PI / 2;
+  float secAngle = seconds * 6 * PI / 180 - PI / 2;
+  float minAngle = (minutes + seconds / 60.0) * 6 * PI / 180 - PI / 2;
+  float hourAngle = ((hours % 12) + minutes / 60.0) * 30 * PI / 180 - PI / 2;
 
   display.clearDisplay();
 
-  // Draw clock border
   display.drawCircle(centerX, centerY, 30, WHITE);
-
-  // Draw hour, minute, and second hands
-  drawHand(centerX, centerY, hourAngle, 12);   // Shorter hour hand
-  drawHand(centerX, centerY, minuteAngle, 20); // Medium minute hand
-  drawHand(centerX, centerY, secondAngle, 28); // Long second hand
-
-  // Optional: Draw center dot
   display.fillCircle(centerX, centerY, 2, WHITE);
 
+  drawHand(hourAngle, 12, WHITE);
+  drawHand(minAngle, 20, WHITE);
+  drawHand(secAngle, 28, WHITE);
+
   display.display();
-  delay(1000);
+  delay(50);  // Fast refresh
 }
